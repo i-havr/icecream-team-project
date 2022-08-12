8aa5e11a287d20baf9f399ac3edb60f258ab2327
bishket <106529417+bishket@users.noreply.github.com>
2022-08-12 15:49:17 +0300

    add images large size and webp; add js

---
+++ src/js/gallery.js
@@ -0,0 +1,7 @@
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+
+var lightbox = new SimpleLightbox('.gallery__list a', { 
+    captionsData: 'alt',
+    captionsDelay: 250,
+ });
