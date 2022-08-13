12551bd7cffbea1693af76edd6a59a737196b21e
AleksandraPauk <aleksandra.pauk@ukr.net>
2022-08-13 17:22:35 +0300

    add contact modals

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './js/anchor-scroll';
 import './js/gallery';
 import './js/slick-slider';
-import './js/contacts-modal';
+import './js/contacts-map-modal';
+import './js/contacts-franchise-modal';
