c00c4c26a6e797306ae44ea91b2b81bc1cad1181
AleksandraPauk <aleksandra.pauk@ukr.net>
2022-08-13 19:11:41 +0300

    add fix (js)

--- src/js/contacts-map-modal.js
+++ src/js/contacts-map-modal.js
@@ -2,7 +2,7 @@
     const refs = {
       openModalBtn: document.querySelector("[data-modal-location-open]"),
       closeModalBtn: document.querySelector("[data-modal-location-close]"),
-      modal: document.querySelector("[data-modal]"),
+      modal: document.querySelector("[data-modal-location]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
