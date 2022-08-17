6bf091037ac519460b17700fcbccca9320049c93
i-havr <i.havrylov@gmail.com>
2022-08-17 13:26:22 +0300

    add js in index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './js/anchor-scroll';
+// import './js/anchor-scroll';
 import './js/gallery';
 import './js/slick-slider';
 import './js/about-modal';
 import './js/contacts-map-modal';
 import './js/contacts-franchise-modal';
-import './js/mobile-menu';
+// import './js/mobile-menu';
 import './js/customer-reviews-slick-slider';
 import './js/modal';
